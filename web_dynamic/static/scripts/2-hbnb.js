995a0969f6c1334cd23262a0285550f8f1540bec
Marirosa <arqmvilcherrez@gmail.com>
2023-11-12 20:23:28 -0500

    Modify status request and html

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,20 +1,22 @@
 /* Script that change class to available o remove */
 
-document.addEventListener('DOMContentLoaded', function() {
-    //Create a Ajax request
-    const newRequest = new XMLHttpRequest();
-    newRequest.open('GET', 'http://0.0.0.0:5001/api/v1/status/', true);
-    //Configure the function to manage the response
-    newRequest.onload = function() {
-        if (newRequest.status >= 200 && newRequest.status < 400) {
-            const data = JSON.parse(newRequest.responseText);
-            //If status is "OK" add Class
-            if(data.status === "OK") {
-                $("#api_status").addClass("available");
+$(document).ready(function() {
+    // Ajax request
+    $.ajax({
+        url: 'http://0.0.0.0:5001/api/v1/status/',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            // if the status is ok change the class
+            if (data.status === 'OK') {
+                $('div#api_status').addClass('available');
             } else {
-                $("#api_status").removeClass("available");
+                $('div#api_status').removeClass('available');
             }
-        } 
-    }
-    newRequest.send(); // send the request
+        },
+        error: function(textStatus, errorThrown) {
+            // manage errors
+            console.error('Error making the AJAX request:', textStatus, errorThrown);
+        }
+    });
 });
